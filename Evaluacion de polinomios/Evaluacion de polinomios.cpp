dbfca23af38e55d6251889fd8ec761e05ad365b6
diegosoto233 <diegosotofx123@gmail.com>
2022-12-01 22:32:57 -0600

    Primer switch

--- Evaluacion de polinomios/Evaluacion de polinomios.cpp
+++ Evaluacion de polinomios/Evaluacion de polinomios.cpp
@@ -8,4 +8,11 @@ int main()
 	printf("Opciones: \n");
 	printf("\n1. Introducir el polinomio y evaluarlo.\n2. Multiplicar.\n3. Dividir. ");
 	printf("\nEscogiste: "); scanf_s("%i", &eleccion);
+
+	switch (eleccion)
+	{
+	case 1:printf("\n\tEscogiste: %i", eleccion);
+		printf("\n\nGrado maximo del polinomio?: "); scanf_s("%i", &Max);
+		printf("\nCoeficiente del monomio?");
+	}
 }
